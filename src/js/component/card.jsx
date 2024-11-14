050654564df9018f43df4ea1fcce5b7b1d9bc542
Aaron Pitarch <aapitarch@gmail.com>
2024-11-14 09:42:33 +0000

    actualizacion landing

--- src/js/component/card.jsx
+++ src/js/component/card.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
-function Card() {
+export const Card = ({image, title, description}) => {
     return (
-        <div className="card mb-4" style={{ width: "18rem" }}>
-            <img src="https://via.placeholder.com/500x325" className="card-img-top" alt="..." />
-            <div className="card-body">
-                <h5 className="card-title text-center">Card title</h5>
-                <p className="card-text text-center">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <div className="card mb-4 h-100">
+            <img src={image} className="card-img-top" alt="..." />
+            <div className="card-body d-flex flex-column">
+                <h5 className="card-title text-center">{title}</h5>
+                <p className="card-text text-center">{description}</p>
                 <a href="#" className="btn btn-primary d-flex justify-content-center">Find Out More!</a>
             </div>
         </div> 
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -7,6 +7,29 @@ import Footer from "./footer";
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
+const cards = [
+	{
+		image: "https://thumb.pccomponentes.com/w-530-530/articles/1083/10831488/1302-funko-pop-deadpool-3-deadpool.jpg",
+		title: "Deadpool",
+		description: "Funko Pop Marvel Deadpool"
+	},
+	{
+		image: "https://acdn.mitiendanube.com/stores/001/002/330/products/7653456514-funko-pop-goku-eating-noodles-dragon-ball-710-exclusive-meus-colecionaveis-01-29ca5be8aa0d3081e315834811483310-1024-1024.jpg",
+		title: "Goku",
+		description: "Funko Pop Dragon Ball Goku"
+	},
+	{
+		image: "https://cdn.grupoelcorteingles.es/SGFM/dctm/MEDIA03/202009/23/00183180125979____2__1200x1200.jpg",
+		title: "Pikachu",
+		description: "Funko Pop Pokemon Pikachu"
+	},
+	{
+		image: "https://www.leprechaun.es/31188-large_default/funko-pop-1312-vecna-stranger-things.jpg",
+		title: "Vegna",
+		description: "Funko Pop Stranger Things Vecna"
+	},
+]
+
 //create your first component
 const Home = () => {
 	return (
@@ -14,11 +37,12 @@ const Home = () => {
 			<Navbar />
 			<div className="container">
 				<Jumbotron />
-				<div className="wrapper d-flex mt-4 justify-content-between">
-					<Card />
-					<Card />
-					<Card />
-					<Card />
+				<div className="row mx-auto py-3">
+					{cards.map((item, index) => (
+						<div key={index} className="col-sm-6 col-md-4 col-lg-3 mb-4">
+							<Card image={item.image} title={item.title} description={item.description} />
+						</div>
+					))}
 				</div>
 			</div>
 			<Footer />
